24dd83b567f19f57ee0b97d1bf25f21e601083d0
Roland Schulz <schullzroll@gmail.com>
2019-12-22 09:49:37 +0100

    iel/proj1: Rewrite priklad1.c using helper macros
    Signed-off-by: Roland Schulz <schullzroll@gmail.com>

--- iel/proj1/priklad1.c
+++ iel/proj1/priklad1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "helpermacros.h"
 
 typedef enum {
     A,
@@ -39,44 +40,43 @@ double skupiny[SKUP_CNT][VSTUP_CNT] = {
     {103, 60,380,420,330,440,450,650,410,275}, // G
     {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, // H
 };
-#define MOJE_SKUPINA F
+#define MOJE_SKUPINA A
 
 int main () {
     // vstup
-    double U1 = skupiny[MOJE_SKUPINA][U1_id];
-    double U2 = skupiny[MOJE_SKUPINA][U2_id];
-    double R1 = skupiny[MOJE_SKUPINA][R1_id];
-    double R2 = skupiny[MOJE_SKUPINA][R2_id];
-    double R3 = skupiny[MOJE_SKUPINA][R3_id];
-    double R4 = skupiny[MOJE_SKUPINA][R4_id];
-    double R5 = skupiny[MOJE_SKUPINA][R5_id];
-    double R6 = skupiny[MOJE_SKUPINA][R6_id];
-    double R7 = skupiny[MOJE_SKUPINA][R7_id];
-    double R8 = skupiny[MOJE_SKUPINA][R8_id];
+    _(U1, skupiny[MOJE_SKUPINA][U1_id], "V");
+    _(U2, skupiny[MOJE_SKUPINA][U2_id], "V");
+    _(R1, skupiny[MOJE_SKUPINA][R1_id], "omega");
+    _(R2, skupiny[MOJE_SKUPINA][R2_id], "omega");
+    _(R3, skupiny[MOJE_SKUPINA][R3_id], "omega");
+    _(R4, skupiny[MOJE_SKUPINA][R4_id], "omega");
+    _(R5, skupiny[MOJE_SKUPINA][R5_id], "omega");
+    _(R6, skupiny[MOJE_SKUPINA][R6_id], "omega");
+    _(R7, skupiny[MOJE_SKUPINA][R7_id], "omega");
+    _(R8, skupiny[MOJE_SKUPINA][R8_id], "omega");
 
     // secteni zdroju
-    double U12 = U1 + U2;
-    printf("U12 = %lf [V]\n", U12);
+    _(U12, U1 + U2, "V");
 
     // trojuhelnik -> hvezda
-    double RA = (R1 * R2) / (R1 + R2 + R3);
-    double RB = (R1 * R3) / (R1 + R2 + R3);
-    double RC = (R2 * R3) / (R1 + R2 + R3);
+    _(RA, (R1 * R2) / (R1 + R2 + R3), "omega");
+    _(RB, (R1 * R3) / (R1 + R2 + R3), "omega");
+    _(RC, (R2 * R3) / (R1 + R2 + R3), "omega");
 
-    double RC6 = RC + R6;
-    double RB4 = RB + R4;
-    double RB45 = RB4 + R5;
-    double RB45C6 = (RB45 * RC6) / (RB45 + RC6);
-    double R78 = (R7 * R8) / (R7 + R8);
-    double Rekv = RA + RB45C6 + R78;
-    double I = U12/Rekv;
-    double URB45C6 = I * RB45C6;
-    double IRB45 = URB45C6 / RB45;
-    double UR5 = IRB45 * R5;
-    double IR5 = IRB45;
+    _(RC6, RC + R6, "omega");
+    _(RB4, RB + R4, "omega");
+    _(RB45, RB4 + R5, "omega");
+    _(RB45C6, (RB45 * RC6) / (RB45 + RC6), "omega");
+    _(R78, (R7 * R8) / (R7 + R8), "omega");
+    _(Rekv, RA + RB45C6 + R78, "omega");
+    _(I, U12/Rekv, "A");
+    _(URB45C6, I * RB45C6, "V");
+    _(IRB45, URB45C6 / RB45, "A");
+    _(IRC6, URB45C6/RC6, "A");
 
     printf("Vysledek:\n");
-    printf("UR5 = %lf [V]\n IR5 = %lf[A]\n",UR5,IR5);
-return 0;
-}
+    _(UR5, IRB45 * R5,"V");
+    _(IR5, IRB45, "A");
 
+    return 0;
+}
