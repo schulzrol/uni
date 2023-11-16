3f28abed7a6ddcd66b71db5bdc09c93a1274c567
Roland Schulz <schullzroll@gmail.com>
2023-11-16 19:46:23 +0100

    feat: add average seed and cloudMethod

--- imp/imp-tachometr/src/main.cpp
+++ imp/imp-tachometr/src/main.cpp
@@ -3,14 +3,23 @@
 #include <Adafruit_SSD1306.h>
 #include <Preferences.h>
 #include <WebServer.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-char ssid[] = "tachometr";  // network SSID (name)
-char pass[] = "tachometr";  // network password
+HTTPClient http;
+String cloudMethod = "";
+String asStationSSID = "iPhone";  // network SSID (name)
+String asStationPASS = "12345678";  // network SSID (name)
+
+char asAPSSID[] = "tachometr";  // network SSID (name)
+char asAPPass[] = "tachometr";  // network password
 char hostname[] = "tachometr";  // arduino hostname
 bool hideSSID = false;       // whether to NOT broadcast SSID
 
+
+String IP = "";
 int status = WL_IDLE_STATUS;
-String ip;
+unsigned int tries = 0;
 WebServer server(80);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -38,22 +47,32 @@ hw_timer_t *refreshDisplayTimer = NULL;
 #define menuButton 25
 #define tabHeight 10
 #define margin 2
+#define AVG_OVER 10
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-typedef enum menuState {kmphMenu, mphMenu, distanceMenu, RPMMenu, serverMenu, menusCount} menuStateT;
-menuStateT nextMenuState[] = {mphMenu, distanceMenu, RPMMenu, serverMenu, kmphMenu};
-int menuState = menuState::kmphMenu;
 
-typedef enum serverState         {serverOn, serverClose, serverOff, serverStartup} serverStateT;
-serverStateT nextServerState[] = {serverClose, serverOff, serverStartup, serverOn};
+typedef enum menuState {speedMenu, distanceMenu, RPMMenu, serverMenu, menusCount} menuStateT;
+menuStateT nextMenuState[] = {distanceMenu, RPMMenu, serverMenu, speedMenu};
+int menuState = menuState::speedMenu;
+
+typedef enum serverState         {serverAsAP, serverAsStationConnecting, serverAsStation, serverClose, serverOff, serverAsAPStartup} serverStateT;
+serverStateT nextServerState[] = {serverAsStationConnecting, serverAsStation, serverClose, serverOff, serverAsAPStartup, serverAsAP};
 int serverState = serverState::serverOff;
+
+unsigned long lastDataSentMillis = 0;
 
 int rotations = 0;
 bool APEnabled = false;
 
+bool memoryFull = false;
+int memoryPosition = 0;
+double lastKmphSpeeds[AVG_OVER] = {0};
+
+volatile bool useMph = false;
 unsigned int connected_devices = 0;
 auto prefs = Preferences();
+volatile double averageSpeedKmph = 0;
 volatile double distance = 0;
 volatile bool eraseDistance = false;
 volatile bool rotated = false;
@@ -80,6 +99,24 @@ double getSpeedKmph(double RPM, double wheelDiameterCm) {
     return (RPM * wheelDiameterCm * PI * 60) / 100000; // 100000 cm in a km and 60 minutes in an hour
 }
 
+double updateAverageSpeedKmph(double newSpeed, double memory[AVG_OVER]=lastKmphSpeeds, int* position=&memoryPosition, bool* full=&memoryFull) {
+    int n = (*full) ? AVG_OVER : (*position)+1;
+    double sum = 0;
+
+    memory[*position] = newSpeed;
+
+    for (int i = 0; i < n; i++) {
+        sum += memory[i];
+    }
+    if ((*position)+1 == AVG_OVER) {
+        *full = true;
+    }
+    *position = ((*position)+1) % AVG_OVER;
+    double average = std::max(sum, 0.) / (double) n;
+    averageSpeedKmph = average;
+    return average;
+}   
+
 void rotationHandler(){
     unsigned long currentTime = millis();
     delayMicroseconds(debounce_delay_micros);
@@ -104,7 +141,18 @@ void menuReleaseHandler(){
             eraseDistance = true;
         }
         else if (wasLongPress && (menuState == serverMenu)){
-            serverState = nextServerState[serverState];
+            if (serverState == serverAsStationConnecting) {
+                serverState = serverClose;
+            }
+            else if (serverState == serverClose){
+                // do nothing
+            }
+            else {
+                serverState = nextServerState[serverState];
+            }
+        }
+        else if (wasLongPress && (menuState == speedMenu)){
+            useMph = !useMph;
         }
         else {
             menuState = nextMenuState[menuState];
@@ -121,10 +169,6 @@ void menuPressHandler(){
         lastMenuPressTime = currentTime;
         attachInterrupt(digitalPinToInterrupt(menuButton), menuReleaseHandler, RISING);
     }
-}
-
-unsigned int max(unsigned int a, unsigned int b){
-    return a > b ? a : b;
 }
 
 void showTabs(){
@@ -146,41 +190,60 @@ void showTabs(){
 
 }
 
-void printLabeledValue(double value, String label, String format="%5.1f"){
+void printLabeledValue(double value, String label, String format="%5.1f", double* labelValue=NULL){
     // center the speed in the middle of the screen
     display.setCursor(0, 0);
     display.setTextSize(4);
     display.setTextColor(SSD1306_WHITE);
     display.printf(format.c_str(), value);
     // draw at the right edge of the screen below the speed
-    int length = label.length();
-    display.setCursor(SCREEN_WIDTH - (length*2*6), 32);
     display.setTextSize(2);
-    display.printf(label.c_str());
+    if (labelValue != NULL) {
+        String separator = ";;";
+        int separatorPosition = label.indexOf(separator);
+        String valueFmt = label.substring(0, separatorPosition);
+        String descLabel = label.substring(separatorPosition + separator.length());
+        display.setCursor(0, 32);
+        display.printf(valueFmt.c_str(), *labelValue);
+        display.setCursor(SCREEN_WIDTH - (descLabel.length()*2*6), 32);
+        display.printf(descLabel.c_str());
+    }
+    else {
+        display.setCursor(SCREEN_WIDTH - (label.length()*2*6), 32);
+        display.printf(label.c_str());
+    }
 }
 
 void showMenu() {
     unsigned long currentTime = millis();
     unsigned long sinceLastRotation = currentTime - lastRotationTime;
+    unsigned long lastRTD = lastRotationTimeDifference;
 
-    unsigned long timeDifference = max(lastRotationTimeDifference, sinceLastRotation);
+    unsigned long timeDifference = std::max(lastRTD, sinceLastRotation);
     double RPM = RPMFromTimeDifference(timeDifference);
     display.clearDisplay();
     showTabs();
-
+    double kmphSpeed = getSpeedKmph(RPM, diameter);
+    double avgKmphSpeed = updateAverageSpeedKmph(kmphSpeed);
     switch (menuState) {
-        case kmphMenu: {
-            double kmph = getSpeedKmph(RPM, diameter);
-            printLabeledValue(kmph, "Km/h");
+        case speedMenu: {
+            double speed, avgSpeed;
+            String avgLabel;
+            if (useMph) {
+                avgSpeed = KmphToMph(avgKmphSpeed);
+                speed = KmphToMph(kmphSpeed);
+                avgLabel = "%5.1f;;Mph";
+            }
+            else {
+                avgSpeed = avgKmphSpeed;
+                speed = kmphSpeed;
+                avgLabel = "%5.1f;;Km/h";
+            }
+            printLabeledValue(speed, avgLabel, "%5.1f", &avgSpeed);
             display.display();
             break;
         }
-        case mphMenu: {
-            double mph = KmphToMph(getSpeedKmph(RPM, diameter));
-            printLabeledValue(mph, "Mph");
-            display.display();
-            break;
-        }
+
         case distanceMenu: {
             double in_meters = distance/100;
             double in_km = in_meters/1000;
@@ -211,48 +274,114 @@ void showMenu() {
             break;
         }
         case serverMenu: {
-            if (serverState == serverOn) {
-                // print ssid and password
-                display.setCursor(0,0);
-                display.setTextSize(1);
-                display.setTextColor(SSD1306_WHITE);
-                display.printf("SSID: %s", ssid);
-                display.setCursor(0,10);
-                display.printf("PASS: %s", pass);
-                display.setCursor(0,20);
-                display.printf("IP: %s", ip);
-                display.setCursor(0,30);
-                display.printf("Connected devices: %d", connected_devices);
-                display.display();
-                break;
+            switch (serverState) {
+                case serverAsAP: {
+                    // print ssid and password
+                    display.setCursor(0,0);
+                    display.setTextSize(1);
+                    display.setTextColor(SSD1306_WHITE);
+                    display.printf("SSID: %s", asAPSSID);
+                    display.setCursor(0,10);
+                    display.printf("PASS: %s", asAPPass);
+                    display.setCursor(0,20);
+                    display.printf("IP: %s", IP.c_str());
+                    display.setCursor(0,30);
+                    display.printf("Connected devices: %d", connected_devices);
+                    display.display();
+                    break;
+                }
+                case serverAsStationConnecting: {
+                    display.setCursor(0,0);
+                    display.setTextSize(1);
+                    display.setTextColor(SSD1306_WHITE);
+                    display.printf("SSID: %s", asStationSSID);
+                    display.setCursor(0,10);
+                    display.printf("PASS: %s", asStationPASS);
+                    display.setCursor(0,20);
+                    display.printf("Connecting... %d", tries);
+                    display.display();
+                    break;
+                }
+                
+                case serverAsStation: {
+                    display.setCursor(0,0);
+                    display.setTextSize(1);
+                    display.setTextColor(SSD1306_WHITE);
+                    display.printf("Connected to %s", asStationSSID);
+                    display.setCursor(0,10);
+                    display.printf("IP: %s", IP.c_str());
+                    display.setCursor(0,20);
+                    display.printf("Method: ");
+                    display.setCursor(0,30);
+                    display.printf("%s", cloudMethod.c_str());
+                    display.display();
+                    break;
+                }
+                
+                case serverClose: {
+                    printLabeledValue(0, "Closing...", "");
+                    display.display();
+                    break;
+                }
+                
+                default: {
+                    printLabeledValue(0, "SERVER OFF", "");
+                    display.display();
+                    break;
+                }
             }
-            else {
-                printLabeledValue(0, "AP OFF", "");
-                display.display();
-                break;
-            }
+            break;
         }
     }
 }
 
 void initDataFromPersistent(){
-    bool success = prefs.begin("distance", false);
-    if (!success) {
+    if (!prefs.begin("tachometr", false)) {
         Serial.println("Failed to open preferences");
         for(;;); // Don't proceed, loop forever
     }
+
+    if (prefs.isKey("stationSSID")) {
+        Serial.println("Found stationSSID key");
+        asStationSSID = prefs.getString("stationSSID", asStationSSID);
+    }
+    else {
+        Serial.println("Did not find stationSSID key. Initializing.");
+        prefs.putString("stationSSID", asStationSSID);
+    }
+
+    if (prefs.isKey("stationPASS")) {
+        Serial.println("Found stationPASS key");
+        asStationPASS = prefs.getString("stationPASS", asStationPASS);
+    }
+    else {
+        Serial.println("Did not find stationPASS key. Initializing.");
+        prefs.putString("stationPASS", asStationPASS);
+    }
+
     if (prefs.isKey("distance")) {
         Serial.println("Found distance key");
         distance = prefs.getDouble("distance", 0);
     }
     else {
-        Serial.println("Did not find distance key");
+        Serial.println("Did not find distance key. Initializing.");
         prefs.putDouble("distance", 0);
+    }
+
+    if (prefs.isKey("cloudMethod")) {
+        Serial.println("Found cloudMethod key");
+        cloudMethod = prefs.getString("cloudMethod", cloudMethod);
+    }
+    else {
+        Serial.println("Did not find cloudMethod key. Leaving empty.");
     }
 }
 
 void saveDataToPersistent(){
     prefs.putDouble("distance", distance);
+    prefs.putString("stationSSID", asStationSSID);
+    prefs.putString("stationPASS", asStationPASS);
+    prefs.putString("cloudMethod", cloudMethod);
 }
 
 void eraseDistanceHandler(){
@@ -262,20 +391,63 @@ void eraseDistanceHandler(){
 }
 
 void adminPageHandler(){
-    server.send(200, "text/html", "<h1>Tachometr Admin</h1><p>Traveled " + String(distance/100) + " meters. </p><p><a href=\"/erase_distance\">Erase distance</a></p>");
+    double AvgSpeedKmph = averageSpeedKmph;
+    server.send(200, "text/html",
+    "<h1>Tachometr Admin</h1>" \
+    "<p>Traveled " + String(distance/100) + " meters.</p>" \
+    "<p>Avg speed " + String(AvgSpeedKmph) + " Km/h. </p>" \
+    "<p><a href=\"/setup\">Setup</a></p>" \
+    "<p><a href=\"/api\">Access API</a></p>" \
+    "<p><a href=\"/erase_distance\">Erase distance</a></p>");
+}
+
+String dataAsJson(){
+    return
+    "{" \
+    "\"distanceCM\": " + String(distance) + ", " \
+    "\"avgSpeedKmph\": " + String(averageSpeedKmph) + "" \
+    "\"}";
+}
+
+void apiHandler(){
+    server.send(200, "text/json", dataAsJson());
+}
+
+void setupHandler(){
+    if (server.args() > 0){
+        if (server.hasArg("stationSSID")) { // ssid
+            String ssid = server.arg("stationSSID");
+            asStationSSID = ssid;
+        }
+        if (server.hasArg("stationPASS")) { // pass
+            String pass = server.arg("stationPASS");
+            asStationPASS = pass;
+        }
+        if (server.hasArg("cloudMethod")) { // pass
+            String method = server.arg("cloudMethod");
+            cloudMethod = method;
+        }
+
+        saveDataToPersistent();
+        server.send(200, "text/html", "<h1>Setup saved</h1><p><a href=\"/\">Back</a></p>");
+    } else {
+        server.send(200, "text/html",
+        "<h1>Tachometer as station setup</h1>" \
+        "<form action=\"/setup\" method=\"POST\">" \
+        "<label for=\"cloudMethod\">Cloud Method:</label><br>" \
+        "<input type=\"text\" id=\"cloudMethod\" name=\"cloudMethod\" value=\"" + cloudMethod + "\"><br>" \
+        "<label for=\"stationSSID\">SSID:</label><br>" \
+        "<input type=\"text\" id=\"stationSSID\" name=\"stationSSID\" value=\"" + asStationSSID + "\"><br>" \
+        "<label for=\"stationPASS\">Password:</label><br>" \
+        "<input type=\"text\" id=\"stationPASS\" name=\"stationPASS\" value=\"" + asStationPASS + "\"><br><br>" \
+        "<input type=\"submit\" value=\"Submit\">" \
+        "</form>");
+    }
 }
 
 void setup() {
     Serial.begin(9600);
     initDataFromPersistent();
-
-    // attempt to connect to Wifi network:
-    int retries = 3;
-
-    WiFi.setHostname(hostname);
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, pass, 1, hideSSID);
-    WiFi.enableAP(serverOn);
 
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     if (!display.begin(SSD1306_SWITCHCAPVCC))
@@ -285,8 +457,15 @@ void setup() {
             ; // Don't proceed, loop forever
     }
 
+
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.persistent(false);
+    WiFi.setAutoReconnect(false);
+
     Serial.println(String(getSpeedKmph(60, diameter)));
+    server.on("/setup", setupHandler);
     server.on("/erase_distance", eraseDistanceHandler);
+    server.on("/api", apiHandler);
     server.on("/", adminPageHandler);
     pinMode(rotationButton, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(rotationButton), rotationHandler, FALLING);
@@ -305,36 +484,74 @@ void setup() {
 
 void loop() {
     if (rotated || eraseDistance) {
+        Serial.print("State :" + String(serverState) + "\n");
         saveDataToPersistent();
         rotated = false;
         eraseDistance = false;
     }
-
     switch (serverState){
-        case serverOn: {
+        case serverOff: {
+            break;
+        }
+        case serverAsAPStartup: {
+            WiFi.enableAP(true);
+            WiFi.softAP(asAPSSID, asAPPass, 1, hideSSID);
+            server.begin(80);
+            IP = WiFi.softAPIP().toString();
+            serverState = nextServerState[serverState];
+            break;
+        }
+
+        case serverAsAP: {
             connected_devices = WiFi.softAPgetStationNum();
             server.handleClient();
             break;
         }
-        case serverOff: {
-            break;
-        }
-        case serverStartup: {
-            WiFi.enableAP(true);
-            server.begin(80);
-            ip = WiFi.softAPIP().toString();
-
+        
+        case serverAsStationConnecting: {
+            WiFi.enableAP(false);
+            WiFi.enableSTA(true);
+            WiFi.begin(asStationSSID, asStationPASS);
+            while (WiFi.status() != WL_CONNECTED) {
+                if (tries > 120) { // 120 * 500ms = 60s
+                    serverState = serverClose;
+                    break;
+                }
+                tries++;
+                delay(500);
+            }
+            tries = 0;
+            IP = WiFi.localIP().toString();
             serverState = nextServerState[serverState];
             break;
         }
+
+        case serverAsStation: {
+            server.handleClient();
+            // when lose connection, go back to connecting
+            if (WiFi.status() != WL_CONNECTED) {
+                serverState = serverAsStationConnecting;
+            }
+
+            String httpRequestData = dataAsJson();
+            unsigned long currentTimeMillis = millis();
+            if ((currentTimeMillis - lastDataSentMillis) >= 1000) {
+                http.begin(cloudMethod);
+                http.addHeader("Content-Type", "application/json");
+                int httpResponseCode = http.POST(httpRequestData);
+                http.end();
+                lastDataSentMillis = millis();
+            }
+            break;
+        }
+
         case serverClose: {
-            server.close();
             WiFi.enableAP(false);
+            WiFi.enableSTA(false);
             serverState = nextServerState[serverState];
             break;
         }
     }
-
 
 }
 
